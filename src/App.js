3f37c66ff5cf0e64061e0f3acd206ab407a279b0
Matheus Canovas <matheuscanovas9@gmail.com>
2024-01-05 15:43:29 -0300

    Upando projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [races, setRaces] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8080/doguinhos")
+      .then((response) => response.json())
+      .then((data) => {
+        setRaces(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (search && search.length > 3) {
+      fetch(`http://localhost:8080/doguinhos?name=${search}`)
+        .then((response) => response.json())
+        .then((data) => {
+          setRaces(data);
+        });
+    }
+  }, [search]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bem-vindo aos doguinhos!</h1>
+      <h4>Confira abaixo uma lista de raças dos doguinhos</h4>
+      <input
+        placeholder="Buscar por raças"
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
+      <ul>
+        {races.map((race) => (
+          <li key={race.name}>{race.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
